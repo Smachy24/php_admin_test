2d16b8f560cb27a666b5fcbe3282dba62f09b12a
Diego Carvalho <pro.diegocdossantos@gmail.com>
2022-10-21 15:46:00 +0200

    fin delete

--- init/db.php
+++ init/db.php
@@ -1,10 +1,71 @@
 <?php
 
 // connexion a la DB
+class BDD{
 
-$db = new PDO('mysql:...', 'root', '');
+  private $connexion;
+  private $listUsers;
+
+  public function __construct(){
+    $this -> connect();
+    $this ->listUsers = [];
+  }
+  
+
+  function getUsers(){
+    return $this -> listUsers;
+  }
+
+  function connect(){
+    
+    try {
+      if ($this->connexion === null) {
+      
+        $this -> connexion = new PDO('mysql:host=localhost;dbname=controle', 'root', '');
+        $this -> connexion -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        
+        }
+      } 
+    catch (PDOException $e) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      die();
+
+    }
+  }
+
+  function selectUsers(){
+    $sql = "SELECT * FROM Users";
+    $req = $this -> connexion -> prepare($sql);
+    $req -> execute();
+    $all = $req -> fetchAll();
+    
+
+    foreach($all as $row){
+      $array = [$row['id'],$row['username'], $row['password'], $row['created_at'] , $row['updated_at']];
+      array_push($this -> listUsers, $array);
+
+    }
+  }
+  function deleteUser($id){
+    $sql = "DELETE FROM Users WHERE id =".$id;
+    $req = $this -> connexion -> prepare($sql);
+    $req -> execute();
+    $req -> fetchAll();
+  }
+
+  function addUser($username, $password){
+    $sql = "INSERT INTO Users(username, password) VALUES(\"".$username."\",\"".$password."\")";
+    $req = $this -> connexion -> prepare($sql);
+    $req -> execute();
+    $req -> fetchAll();
+  }
+
+}
 
 // par défaut, les SELECT FROM  -> fetch et fetchAll recupere des tableaux associatifs
 // possible de recup des objets en changeant PDO::FETCH_ASSOC par PDO::FETCH_OBJ
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+
+
+$bd = new Bdd();
+$bd -> selectUsers();
--- users.php
+++ users.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__ . '/init/db.php';
+//require_once __DIR__ . '/init/db.php';
+include "init/db.php";
 
 // if pour la story 4
 if (isset($_GET['username'])) {
@@ -14,6 +15,7 @@ $stmt->execute();
 $users = $stmt->fetchAll();
 */
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,6 +29,75 @@ $users = $stmt->fetchAll();
     <div></div>
 
     <!-- Table des Utilisateurs -->
-    <div></div>
+    <div>
+        <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>username</th>
+                <th>password</th>
+                <th>created_at</th>
+                <th>updated_at</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                
+                
+
+                for ($a = 0; $a < count($bd->getUsers()); $a++) { 
+                    echo "<tr>";
+                    for ($b = 0; $b < count($bd->getUsers()[$a]); $b++) {
+                    echo "<td>" . $bd->getUsers()[$a][$b] . "</td>";
+                    
+                    }
+                    
+                    
+                    
+
+                    echo "<form method = 'post'>";
+                    echo "<td><button type=submit name ='delete'>Delete</button></td>";
+                    echo "<td><button form ='post' name ='a'>Update</button></td>";
+                    echo "</form>";
+                    echo "</tr>";
+
+                    if(isset($_POST["delete"])){
+                        $user_id = $bd->getUsers()[$a][0];
+                        echo $user_id;
+                    }
+                }
+
+            ?>
+
+
+                
+        </tbody>
+        </table>
+
+    </div>
+
+       
+
+    <form method="post" action="users.php">
+
+        <input name="username" placeholder="username">
+        <input name="password" placeholder="password">
+        <button type="submit" name="create">Create</button>
+
+    </form>
+
+
+    <?php
+        
+        if(isset($_POST["create"])){
+            if(!empty($_POST["username"]) && !empty($_POST["password"])){
+                $bd ->addUser($_POST["username"], $_POST["password"]);
+            }
+            
+        }   
+        
+
+    ?>
 </body>
 </html>
